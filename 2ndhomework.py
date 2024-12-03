0735079a7aec5b904c52bf22a0764d75044ba54c
Username387J <jdeleijagbulu@gmail.com>
2024-12-02 18:54:42 -0700

    Add files via upload

---
+++ 2ndhomework.py
@@ -0,0 +1,12 @@
+import random
+from random import randint
+
+matrix = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],[31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60],[61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]]
+matrix1 = [["Student 1", "Student 2", "Student 3", "Student 4", "Student 5", "Student 6", "Student 7", "Student 8", "Student 9", "Student 10", "Student 11", "Student 12", "Student 13", "Student 14", "Student 15", "Student 16", "Student 17", "Student 18", "Student 19", "Student 20"]]
+
+
+
+for i in range (20):
+    randomperson = random.choice(matrix1[0])
+    randomscore = randint(1, 100)
+    print(randomperson, "got", randomscore)
